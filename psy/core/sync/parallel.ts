81b007a561c34c3e40943d58019790596cacc94f
Stefan Zerkalica <zerkalica@gmail.com>
2021-06-12 00:35:56 +0300

    build: deps

--- psy/core/sync/parallel.ts
+++ psy/core/sync/parallel.ts
@@ -1,5 +1,6 @@
 import { psyDataIsPromise } from '../data/isPromise'
 import { psyErrorThrowHidden } from '../error/hidden'
+import { PsyErrorMix } from '../error/Mix'
 
 type Primitive = string | number | boolean | object
 type PackedBase = Record<string, Primitive | Primitive[]>
@@ -22,15 +23,18 @@ export function psySyncParallel<V extends PackedBase>(v: Wrapped<V>): V {
   const keys = Object.keys(v)
   const result = {} as V
   const promises: PromiseLike<any>[] = []
+  const errors = [] as Error[]
+
   for (let key of keys) {
     try {
       result[key as keyof V] = v[key]()
     } catch (error) {
       if (psyDataIsPromise(error)) promises.push(error)
-      else return psyErrorThrowHidden(error)
+      else errors.push(error)
     }
   }
 
+  if (errors.length > 0) return psyErrorThrowHidden(errors.length === 1 ? errors[0] : new PsyErrorMix('Parallel', errors))
   if (promises.length > 0) return psyErrorThrowHidden(Promise.all(promises))
 
   return result
